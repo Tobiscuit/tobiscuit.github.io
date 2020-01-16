ec6f912a73500848ebd16edca233fa68db319032
Tobiscuit <33387497+Tobiscuit@users.noreply.github.com>
2020-01-16 00:57:14 -0600

    Add files via upload
    Logging JavaScript DOM assignment for Jan 15 2020

---
+++ dom-assignment-completed.js
@@ -0,0 +1,17 @@
+const taskEl1 = document.getElementById('task-1');
+const taskEl2 = document.querySelector('li');
+
+task1El1.style.color = 'white';
+task1El1.style.backgroundColor = 'black';
+
+const docTitle1 = document.querySelector('title');
+docTitle1.textContent = 'Assignment - Solved!';
+
+const docHead = document.head;
+docTitle2 = document.head.querySelector('title');
+
+docTitle2.textContent = 'Assignment - Solved';
+
+// const h1 = document.querySelector('h1');
+const h1 = document.getElementsByTagName('h1');
+h1[0].textContent = 'Assignment - Solved!';

ac6524fe72299ad317289381cfc261b7840271fe
tobiii <33387497+Tobiscuit@users.noreply.github.com>
2020-03-31 23:43:34 -0500

    Add files via upload
    JS30: HTML5 Video Player
    Had to delete the video, and the project has not been finished. Researching VSCode extensions to provide CSS autocompletion while working on a JS file. Wes Bos has autocomplete after typing .style and I'd like the same functionality.
    Note to self: had to remove the video due to the 25mb file size limit. Will update project folder tomorrow with finished project and an alternate, shorter video.

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,57 @@
+// Get our elements
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Built our functions
+function togglePlay() {
+  //   if (video.paused) {
+  //     video.play();
+  //   } else {
+  //     video.pause();
+  //   }
+  // Ternary operator variation below
+
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
+  toggle.textContent = icon;
+}
+
+function skip() {
+  //   console.log(this.dataset.skip);
+  video.currentTime += +this.dataset.skip;
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  //   console.log(this.name);
+  //   console.log(this.value);
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style;
+  // Pausing here for today 3/31/2020
+  // Noticed Wes Bos has autocompletion for progressBar.styles./w/
+  // Researching extensions for CSS / JS to give me the same functionality
+}
+
+// Hook up the event listerners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
